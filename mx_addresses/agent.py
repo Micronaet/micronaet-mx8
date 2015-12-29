dcd5a616fbe10452cb904e4e64bf88ce30219d9c
nicothebrush <nicola.riolini@gmail.com>
2015-12-29 18:58:10 +0100

    [ADD] mx_addresses: start module for add address elements in form

---
+++ mx_addresses/agent.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+import os
+import sys
+import logging
+import openerp
+import openerp.netsvc as netsvc
+import openerp.addons.decimal_precision as dp
+from openerp.osv import fields, osv, expression, orm
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from openerp import SUPERUSER_ID, api
+from openerp import tools
+from openerp.tools.translate import _
+from openerp.tools.float_utils import float_round as round
+from openerp.tools import (DEFAULT_SERVER_DATE_FORMAT, 
+    DEFAULT_SERVER_DATETIME_FORMAT, 
+    DATETIME_FORMATS_MAP, 
+    float_compare)
+
+
+_logger = logging.getLogger(__name__)
+
+class SaleOrder(orm.Model):
+    """ Model name: Sale Order
+    """    
+    _inherit = 'sale.order'
+    
+    # TODO onchange for setup from partner
+    
+    _columns = {
+        'mx_agent_id': fields.many2one('res.partner', 'Agent', 
+            domain=[('is_agent', '=', True)]),
+        }
+
+class AccountInvoice(orm.Model):
+    """ Model name: Account Invoice
+    """    
+    _inherit = 'account.invoice'
+    
+    # TODO onchange for setup from partner
+    
+    _columns = {
+        'mx_agent_id': fields.many2one('res.partner', 'Agent', 
+            domain=[('is_agent', '=', True)]),
+        }
+
+class StockPicking(orm.Model):
+    """ Model name: Stock Picking    
+    """    
+    _inherit = 'stock.picking'
+    
+    # TODO onchange for setup from partner
+    
+    _columns = {
+        'mx_agent_id': fields.many2one('res.partner', 'Agent', 
+            domain=[('is_agent', '=', True)]),
+        }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
