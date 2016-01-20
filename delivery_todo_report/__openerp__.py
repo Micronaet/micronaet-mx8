537ed94d1b98785db72cc0783493a54e9af72c37
nicothebrush <nicola.riolini@gmail.com>
2016-01-20 08:10:37 +0100

    [ADD] delivery_todo_report: for print a list of order details for delivery report Moved in new module report for delivery

---
+++ delivery_todo_report/__openerp__.py
@@ -0,0 +1,44 @@
+###############################################################################
+#
+#    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Delivery todo report',
+    'version': '0.1',
+    'category': 'Report',
+    'description': ''' 
+        Add print button to sale order for print a list of delivery 
+        (and a sort of order for stock user)       
+        ''',
+    'author': 'Micronaet S.r.l. - Nicola Riolini',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'sale',
+        ],
+    'init_xml': [],
+    'demo': [],
+    'data': [
+        #'security/ir.model.access.csv',    
+        'delivery_view.xml',
+        ],
+    'active': False,
+    'installable': True,
+    'auto_install': False,
+    }
