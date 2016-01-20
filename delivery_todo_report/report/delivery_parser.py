f3002973bf7df53baf6dfa68f3d778e4d4f1f801
nicothebrush <nicola.riolini@gmail.com>
2016-01-20 11:25:21 +0100

    Delivery in ODS not ODT (leave olt ODT)

--- delivery_todo_report/report/delivery_parser.py
+++ delivery_todo_report/report/delivery_parser.py
@@ -23,6 +23,7 @@
 ##############################################################################
 from openerp.report import report_sxw
 from openerp.report.report_sxw import rml_parse
+from datetime import datetime, timedelta
 
 
 class Parser(report_sxw.rml_parse):
@@ -36,16 +37,36 @@ class Parser(report_sxw.rml_parse):
             'get_counter': self.get_counter,
             'set_counter': self.set_counter,
 
+            'get_object_line': self.get_object_line,
+            'get_datetime': self.get_datetime,
+            'get_partic': self.get_partic,
+
             # TODO needed?            
-            'product_list': self.product_list,
             'colli_total': self.colli_total,
             'orders': self.order_browse,
             'total_orders': self.total_orders, 
             'is_last': self.is_last,
             'reset_print': self.reset_print,
-            'get_telephone': self.get_telephone,
             'get_address_default': self.get_address_default,
         })
+
+    def get_partic(self, line):
+        ''' Return return if present partner-product partic code
+        '''
+        partic_pool = self.pool.get('res.partner.product.partic')
+        partic_ids = partic_pool.search(self.cr, self.uid, [
+            ('partner_id', '=', line.order_id.partner_id.id),
+            ('partner_id', '=', line.product_id.id),
+            ])
+        if partic_ids: 
+            partic_proxy = partic_pool.browse(self.cr, self.uid, partic_ids)[0]
+            return partic_proxy.partner_code or '/'
+        return '???'
+
+    def get_datetime(self):
+        ''' Return datetime obj
+        '''
+        return datetime
 
     def get_counter(self, name):
         ''' Get counter with name passed (else create an empty)
@@ -60,126 +81,49 @@ class Parser(report_sxw.rml_parse):
         self.counters[name] = value
         return "" # empty so no write in module
 
-    def is_last(self, item_id):
-        ''' Test if the record id passed is the last of the list
-        '''
-        if not (item_id and self.last_record):
-            return False
-        return item_id == self.last_record  # return test value
-            
-        if value:
-            return "%s-%s-%s" % (value[8:10], value[5:7], value[:4])
-        return ""        
-
-    def get_italian_date(self, value):
-        if value:
-            return "%s-%s-%s" % (
-                value[8:10],
-                value[5:7],
-                value[:4])
-        return ""
-        
-    def get_telephone(self, ids):
-        if not ids:
-           return ""
-
-        partner_proxy = self.pool.get('res.partner').browse(
-            self.cr, self.uid, ids)
-        #for address in partner_proxy.address:
-        #    if not address.mexal_c and not address.mexal_s:
-        #       return address.phone
-        #return ""
-        return partner_proxy.phone
-
-    def get_address_default(self, item_id, partner_name=""):
-        ''' Get, from id of partner the first default address imported 
-        '''
-        if not item_id:
-            return ""
-        partner_pool = self.pool.get('res.partner')
-        partner_proxy = partner_pool.browse(
-            self.cr, self.uid, item_id)
-        return "%s\n%s\n%s - %s" % (
-            partner_proxy.name or '', 
-            partner_proxy.street or '', 
-            partner_proxy.zip or '', 
-            partner_proxy.city or '',
-            )
-
-    def reset_print(self):
-        ''' Called at the end of report to reset print check
-        '''
-        header_pool = self.pool.get('statistic.header')
-        # Azzero tutte le selezioni (al termine della stampa:
-        header_ids = header_pool.search(
-            self.cr, self.uid, [('print', '=', True)])
-        header_mod = header_pool.write(
-            self.cr, self.uid, header_ids, {'print': False})     
-        return "" # print nothing    
-
-    def total_orders(self, objects):
-        ''' true if the order list is more than one
-        '''
-        active_ids_list = [x.id for x in objects]        
-        order_ids = self.pool.get('statistic.header').search(
-            self.cr, self.uid, [('print', '=', True),])
-        return len(list(set(active_ids_list + order_ids)) )
-
     def _get_fully_list(self, objects):
         ''' Return list of object browse id list merged with no replication 
             with al record masked for print 
         '''
-        active_ids_list = [x.id for x in objects]        
-        order_ids = self.pool.get('statistic.header').search(
-            self.cr, self.uid, [('print', '=', True),])
-        total_list = list(set(active_ids_list + order_ids))
-        return total_list
-        
-    def order_browse(self, objects):
-        ''' Return only in_pricelist product for print a pricelist
-        '''
-        self.last_record = 0
-        
-        ids = self._get_fully_list(objects)
-        order_proxy = self.pool.get('statistic.header').browse(
-            self.cr, self.uid, ids) 
-        
-        self.last_record = ids[-1] # for extra page
-        return order_proxy
-        
-    def colli_total(self, order_id):
-        order_pool = self.pool.get('statistic.order')
-        total = 0
-        order_line_ids = order_pool.search(
-            self.cr, self.uid, [('header_id', '=', order_id)])
-        
-        for order_line in order_pool.browse(
-                self.cr, self.uid, order_line_ids):
-            if order_line.colli:
-               total += order_line.colli or 0
-            else:
-               total += order_line.quantity or 0
-        return total    
+        sale_pool = self.pool.get('sale.order')
+        active_ids = [x.id for x in objects]        
 
-    def product_list(self, objects):
+        print_ids = sale_pool.search(self.cr, self.uid, [
+            ('print', '=', True),])
+        active_ids.extend(print_ids)    
+        return list(set(active_ids))
+
+    def get_object_line(self, objects):
+        ''' Selected object + print object
+        '''
         products = {}
-        for order in self.pool.get('statistic.header').browse(
+        res = []
+        sale_pool = self.pool.get('sale.order')
+        
+        for order in sale_pool.browse(
                 self.cr, self.uid, self._get_fully_list(objects)):
-            for item in order.line_ids:            
-                if item.line_type != "d": # only article lines
-                    if item.code in products:
-                        products[item.code][0] += item.quantity or 0.0
-                        products[item.code][1] += item.quantity_ok or 0.0
-                        products[item.code][2] = item.article or ""
-                    else:          
-                        products[item.code] = [
-                            item.quantity or 0.0, 
-                            item.quantity_ok or 0.0, 
-                            item.article or "",
-                            ]       
-        products_sorted = []       
-        for k in sorted(products.iterkeys()):
-            products_sorted.append(
-                [k, products[k][0], products[k][1], products[k][2]])
-        return products_sorted
+ 
+            for line in order.order_line:
+                code = line.product_id.default_code
+                if code not in products:
+                    products[code] = []
+                    
+                #res.append(line) # unsorted
+                products[code].append(line)
+        
+        # create a res order by product code
+        for code in sorted(products):
+            res.extend(products[code])        
+        return res
+        
+    def reset_print(self):
+        ''' Called at the end of report to reset print check
+        '''
+        sale_pool = self.pool.get('sale.order')
+        sale_ids = sale_pool.search(
+            self.cr, self.uid, [('print', '=', True)])
+        sale_pool.write(
+            self.cr, self.uid, sale_ids, {'print': False})     
+        return ''
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
