f748ecbc1614babaa8322e19855117212a0a5de6
nicothebrush <nicola.riolini@gmail.com>
2016-01-25 16:31:57 +0100

    Moved in res.partner the procedure

--- mx_stock_status_multicompany/stock_status.py
+++ mx_stock_status_multicompany/stock_status.py
@@ -89,7 +89,151 @@ class ResPartner(orm.Model):
             inventory: contain all dictionaty used for this calculation
             context: context
         '''
+        # pool used:
+        product_pool = self.pool.get('product.product')
+        supplier_pool = self.pool.get('product.supplierinfo')
+        pick_pool = self.pool.get('stock.picking')
+        sale_pool = self.pool.get('sale.order') # XXX maybe not used 
+        sol_pool = self.pool.get('sale.order.line') # XXX maybe not used 
+        # procurements in stock.picking
         
+        # ---------------------------------------------------------------------
+        # Parameter for filters:
+        # ---------------------------------------------------------------------
+        # Exclunde partner list:
+        exclude_partner_ids = []
+        for item in company_proxy.stock_explude_partner_ids:
+            exclude_partner_ids.append(item.id)
+        # Append also this company partner (for inventory)    
+        exclude_partner_ids.append(company_proxy.partner_id.id)
+        
+        # From date:
+        from_date = datetime.now().strftime('%Y-01-01 00:00:00')    
+        to_date = datetime.now().strftime('%Y-12-31 23:59:59')    
+
+        debug_file.write('\n\nExclude partner list:') # XXX DEBUG
+        debug_file.write('%s' % (exclude_partner_ids,)) # XXX DEBUG
+        # ---------------------------------------------------------------------
+        # Get unload picking
+        # ---------------------------------------------------------------------
+        out_picking_type_ids = []
+        for item in company_proxy.stock_report_unload_ids:
+            out_picking_type_ids.append(item.id)
+            
+        pick_ids = pick_pool.search(self.cr, self.uid, [     
+            # type pick filter   
+            ('picking_type_id', 'in', out_picking_type_ids), 
+            
+            # Partner exclusion
+            ('partner_id', 'not in', exclude_partner_ids), 
+            
+            # TODO check data date
+            ('date', '>=', from_date), 
+            ('date', '<=', to_date), 
+            
+            # TODO state filter
+            ])
+
+        debug_file.write('\n\nUnload picking:\n') # XXX  DEBUG           
+        for pick in pick_pool.browse(self.cr, self.uid, pick_ids):
+            debug_file.write('\nPick: %s' % pick.name) # XXX DEBUG
+            for line in pick.move_lines:
+                if line.product_id.id in product_ids: # only supplier prod.
+                    # TODO check state of line??
+                    default_code = line.product_id.default_code
+                    if default_code not in unloads:
+                        unloads[default_code] = line.product_uom_qty
+                    else:    
+                        unloads[default_code] += line.product_uom_qty
+                debug_file.write('Prod.: %s [%s]\n' % (
+                    default_code, line.product_uom_qty)) # XXX DEBUG
+
+        # ---------------------------------------------------------------------
+        # Get unload picking
+        # ---------------------------------------------------------------------
+        in_picking_type_ids = []
+        for item in company_proxy.stock_report_load_ids:
+            in_picking_type_ids.append(item.id)
+            
+        pick_ids = pick_pool.search(self.cr, self.uid, [     
+            # type pick filter   
+            ('picking_type_id', 'in', in_picking_type_ids), 
+            
+            # Partner exclusion
+            ('partner_id', 'not in', exclude_partner_ids), 
+            
+            # check data date
+            ('date', '>=', from_date), # XXX correct for virtual?
+            ('date', '<=', to_date),
+            
+            # TODO state filter
+            ])
+        debug_file.write('\n\nload picking:\n') # XXX DEBUG       
+        for pick in pick_pool.browse(self.cr, self.uid, pick_ids):
+            for line in pick.move_lines:
+                if line.product_id.id in product_ids: # only supplier prod.
+                    # TODO check state of line??                    
+                    default_code = line.product_id.default_code
+                    if line.state == 'assigned': # virtual
+                        _logger.info('\nOF virtual: %s - %s [%s]\n' % (
+                            line.picking_id.name,
+                            default_code,
+                            line.product_uom_qty,
+                            ))
+                        if default_code not in loads:
+                            virtual_loads[default_code] = line.product_uom_qty
+                        else:    
+                            virtual_loads[default_code] += line.product_uom_qty
+                        debug_file.write('Virtual: %s [%s]\n' % (
+                            default_code, line.product_uom_qty)) # XXX DEBUG
+                    elif line.state == 'done':
+                        _logger.info('OF load: %s - %s [%s]\n' % (
+                            line.picking_id.name,
+                            default_code,
+                            line.product_uom_qty,
+                            ))
+                        if default_code not in loads:
+                            loads[default_code] = line.product_uom_qty
+                        else:    
+                            loads[default_code] += line.product_uom_qty
+                        debug_file.write('Load: %s [%s]\n' % (
+                            default_code, line.product_uom_qty)) # XXX DEBUG
+        
+        # ---------------------------------------------------------------------
+        # Get order to delivery
+        # ---------------------------------------------------------------------
+        sol_ids = sol_pool.search(self.cr, self.uid, [
+            ('product_id', 'in', product_ids)])
+            
+        debug_file.write('\n\nOrder remain:\n') # XXX DEBUG
+        for line in sol_pool.browse(self.cr, self.uid, sol_ids):
+            # -------
+            # Header:
+            # -------            
+            # check state:
+            if line.order_id.state in ('cancel', 'draft', 'sent'): #done?
+                continue
+            
+            # ------
+            # Lines:
+            # ------            
+            # Check delivered:
+            remain = line.product_uom_qty - line.delivered_qty
+            if remain <= 0.0:
+                continue
+            
+            default_code = line.product_id.default_code
+            if default_code in orders:
+                orders[default_code] += remain
+            else:
+                orders[default_code] = remain
+            _logger.info('Order considered: %s - %s [%s]\n' % (
+                line.order_id.name,
+                default_code,
+                remain,
+                )) # XXX DEBUG
+            debug_file.write('Product : %s [%s]\n' % (
+                default_code, remain)) # XXX DEBUG
         
         
         
