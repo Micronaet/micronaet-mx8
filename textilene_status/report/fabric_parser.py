682825e4925f39a0e37b50f3e434fb4aa7f00cbf
nicothebrush <nicola.riolini@gmail.com>
2016-01-28 23:30:40 +0100

    Set inventory not from stock move but from inventory_start

--- textilene_status/report/fabric_parser.py
+++ textilene_status/report/fabric_parser.py
@@ -478,15 +478,15 @@ class Parser(report_sxw.rml_parse):
         for key in sorted(products):            
             current = products[key] # readability:
             total = 0.0 # INV 0.0
-            #current[0] = 0.0 # XXX reset inventory (updated in dec. position)
-            #inv_pos = 3
+            # NOTE: INV now is 31/12 next put Sept.
+            inv_pos = 3 # December
             for i in range(0, 12):
                 current[3][i] = round(current[3][i], 0)
                 current[4][i] = round(current[4][i], 0)
                 current[5][i] = round(current[5][i], 0)
                 
-                #if i == inv_pos:
-                #    total += round(current[0], 0) # add inv.
+                if i == inv_pos:
+                    total += round(current[0], 0) # add inv.
                 total += round(
                     current[3][i] + current[4][i] + current[5][i], 0)
                 current[6][i] = total
