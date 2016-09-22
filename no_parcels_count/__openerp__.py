0fdd20e1cc32ff617e3756c81d33882b2c25919a
nicothebrush <nicola.riolini@gmail.com>
2016-09-22 16:59:05 +0200

    no parcels correct ref.

--- no_parcels_count/__openerp__.py
+++ no_parcels_count/__openerp__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ###############################################################################
 #
 #    Copyright (C) 2001-2014 Micronaet SRL (<http://www.micronaet.it>).
@@ -17,7 +16,27 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###############################################################################
-from . import parcels
 
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+{
+    'name': 'Parcels exclude',
+    'version': '0.1',
+    'category': 'Product',
+    'description': '''  
+        Check product that doesn't create parcels      
+        ''',
+    'author': 'Micronaet S.r.l. - Nicola Riolini',
+    'website': 'http://www.micronaet.it',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'product',
+        ],
+    'init_xml': [],
+    'demo': [],
+    'data': [
+        'parcels_view.xml',
+        ],
+    'active': False,
+    'installable': True,
+    'auto_install': False,
+    }
